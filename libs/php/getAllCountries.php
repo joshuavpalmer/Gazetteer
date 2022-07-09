65ffd116a77db4fd0845a392e78e16c3a9fbb732
Joshua Palmer <joshuapalmer@Joshuas-MacBook-Pro.local>
2022-07-09 17:03:03 +0100

    add modals with information populating

---
+++ libs/php/getAllCountries.php
@@ -0,0 +1,29 @@
+<?php 
+
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+$data_as_string = file_get_contents("../js/json/countryBorders.geo.json");
+$data = json_decode($data_as_string, true);
+
+$countries = [];
+for($country = 0; $country < count($data['features']); $country++) {
+array_push($countries,
+(object)[
+'code' => $data['features'][$country]['properties']['iso_a2'], 
+'name' => $data['features'][$country]['properties']['name']]);
+}
+
+usort($countries, function($a, $b){
+    return strcmp($a->name, $b->name);
+});
+
+$output["status"]["code"] = "200";
+$output["status"]["name"] = "ok";
+$output["status"]["description"] = "success";
+
+$output["countryList"] = $countries;
+
+header('Content-type: application/json; charset=UTF-8');
+echo json_encode($output);
+?>
---
+++ libs/php/getPolygon.php
@@ -0,0 +1,27 @@
+<?php 
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+function getCountryPolygons($countrySelected){
+$data_as_string = file_get_contents("../js/json/countryBorders.geo.json");
+$data = json_decode($data_as_string, true);
+    foreach($data['features'] as $country) {
+        if ($country['properties']['iso_a2'] == $countrySelected) {
+        return $country['geometry'];
+        }
+    }
+}
+
+$output["status"]["code"] = "200";
+$output["status"]["name"] = "ok";
+$output["status"]["description"] = "success";
+
+$countrySelected = $_REQUEST['selectedCountry'];
+$output["countryPolygons"] = getCountryPolygons($countrySelected);
+
+header('Content-type: application/json; charset=UTF-8');
+echo json_encode($output);
+
+
+
+?>
